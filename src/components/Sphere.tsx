6ee4ac903dd43a36a97bd7447a18c6e4189f8ef4
Sevinda Perera <sevindacharukaperera@gmail.com>
2024-08-17 20:47:57 +0530

    feat: basics interactions

--- src/components/Sphere.tsx
+++ src/components/Sphere.tsx
@@ -1,5 +1,5 @@
 import { useFrame } from "@react-three/fiber";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Vector3 } from "three";
 
 interface Props {
@@ -14,15 +14,35 @@ interface Props {
 
 const Sphere = ({ position, args, color }: Props) => {
   const ref = useRef<any>();
+  const [isHovered, setIsHovered] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
   useFrame((state, delta) => {
-    ref.current.rotation.x += delta;
-    ref.current.rotation.y += delta * 2;
+    isHovered
+      ? (ref.current.rotation.y += delta * 0.2)
+      : (ref.current.rotation.y += delta * 1);
   });
 
   return (
-    <mesh position={position} ref={ref}>
-      <sphereGeometry args={args} />
-      <meshStandardMaterial color={color} wireframe={true} />
+    <mesh
+      position={position}
+      ref={ref}
+      onPointerEnter={(event) => {
+        event.stopPropagation();
+        setIsHovered(true);
+      }}
+      onPointerLeave={(event) => {
+        event.stopPropagation();
+        setIsHovered(false);
+      }}
+      onClick={() => setIsClicked(!isClicked)}
+      scale={isClicked ? 2 : 1}
+    >
+      <sphereGeometry args={!isHovered ? args : [2, 60, 60]} />
+      <meshStandardMaterial
+        color={!isHovered ? color : "hotpink"}
+        wireframe={true}
+      />
     </mesh>
   );
 };
